29d4d72115efa0bec0c1bd66410336155fc30d2c
Erin <eringlabe@gmail.com>
2017-04-15 07:56:12 -0700

    js without exports

--- JS without exports/index.js
+++ JS without exports/index.js
@@ -1,5 +1,6 @@
 const db = require("./db.js");
-const promptCustomer = require("./customer.js");
+
+var inquirer = require("inquirer");
 
 
 // ===================================
@@ -24,3 +25,56 @@ db.query("SELECT item_id, product_name, price FROM products", function(err, res)
 	}
 	promptCustomer();
 });
+
+
+// PROMPT CUSTOMER 
+// DATABASE QUERIES
+
+var promptCustomer = function() {
+	inquirer.prompt([
+	{
+		type: "input",
+		name: "userItem",
+		message: "What would you like to buy? Enter the 'Item number':"
+	},
+	{
+		type: "input",
+		name: "userUnits",
+		message: "How many would you like to buy?"
+	}
+
+	]).then(function(userResponse) {
+		var itemOrdered = userResponse.userItem;
+		var unitsOrdered = userResponse.userUnits;
+		var queryStock = "SELECT stock_quantity FROM products WHERE ?";
+
+		db.query(queryStock, {item_id: itemOrdered}, function(err, res) {
+			var quantity = res[0].stock_quantity;
+
+
+			// IF STATEMENT
+			if (quantity < unitsOrdered) {
+				console.log("Order can not be completed. Only " + quantity + " left in stock.")
+			} else {
+
+				var queryPrice = "SELECT price FROM products WHERE ?";
+				
+				db.query(queryPrice, {item_id: itemOrdered}, function(err, res) {
+					var price = res[0].price;
+					var total = parseInt(price * unitsOrdered);
+					console.log("Thank you for your order! Total: $" + total);
+				});
+
+				var newQuantity = parseInt(quantity - unitsOrdered);
+				var queryUpdate = "UPDATE products SET stock_quantity = " + newQuantity + " WHERE ?";
+				db.query(queryUpdate, {item_id: itemOrdered}, function(err, res) {});
+
+			}
+
+
+		});
+
+	});
+};
+
+module.exports = promptCustomer;
